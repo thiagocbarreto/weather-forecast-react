210e7c0a40b4a50219a3894b93917f7b866885db
thiagocbarreto <32571922+thiagocbarreto@users.noreply.github.com>
2022-04-21 15:34:36 -0300

    feat: basic layout

--- src/pages/Home/index.test.tsx
+++ src/pages/Home/index.test.tsx
@@ -16,3 +16,26 @@ test('renders app title', () => {
   const titleElement = screen.getByText(/Weather Forecast App/i);
   expect(titleElement).toBeInTheDocument();
 });
+
+test('renders search input', () => {
+  renderComponent();
+  const searchInputElement = screen.getByPlaceholderText(
+    /Find out the forecast at.../i,
+  );
+  expect(searchInputElement).toBeInTheDocument();
+  expect(searchInputElement).toHaveValue('');
+  expect(screen.getByRole('textbox')).toBe(searchInputElement);
+});
+
+test('render search submit button', () => {
+  renderComponent();
+  const searchSubmitButtonElement = screen.getByText(/Search!/i);
+  expect(searchSubmitButtonElement).toBeInTheDocument();
+  expect(screen.getByRole('button')).toBe(searchSubmitButtonElement);
+});
+
+test('render single forecast item', () => {
+  renderComponent();
+  const forecastItemElement = screen.getByText(/Forecast Item/i);
+  expect(forecastItemElement).toBeInTheDocument();
+});
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,6 +8,22 @@ function Home() {
       <header>
         <PageTitle>Weather Forecast App</PageTitle>
       </header>
+      <div>
+        <input type="text" placeholder="Find out the forecast at..." />
+        <button type="submit">Search!</button>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <span>Forecast Item</span>
+            <span>Day of the week: Thursday</span>
+            <span>Date: April, 21st</span>
+            <span>Temperature: 83ºF</span>
+            <span>Icon: ...</span>
+            <span>Short Forecast: ...</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
